41a3593939acb6f3e40462e07f72466d545b2317
Leonardo Santos <leogilberto2010@hotmail.com>
2023-03-06 22:02:25 -0300

    Arquivo com cores

---
+++ src/config/colors.ts
@@ -0,0 +1,7 @@
+export const color01 = '#2E424D';
+export const color02 = '#5B8291';
+export const color03 = '#98DAD9';
+export const color04 = '#EAEBED';
+export const color05 = '#000000';
+
+export const shadow = '#00000077';
